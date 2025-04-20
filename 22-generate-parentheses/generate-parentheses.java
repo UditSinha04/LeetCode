e15ed2f076e4c7454e96bb3960812a3153040b49
UditSinha04 <sinha.udit04@gmail.com>
2025-04-20 15:35:20 +0530

    Time: 1 ms (81.96%) | Memory: 43.8 MB (15.77%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+class Solution {
+    public List<String> generateParenthesis(int n) {
+        List<String> st = new ArrayList<>();
+        helper(n, st, new StringBuilder(), 0, 0);
+        return st;
+    }
+
+    public void helper(int n, List<String> st, StringBuilder output, int OB, int CB){
+        if (OB == CB && OB == n) {
+            st.add(output.toString());
+            return;
+        } 
+        
+        if (OB < n) {
+            helper(n, st, new StringBuilder(output).append('('), OB + 1, CB);
+        }
+
+        if (CB < OB) {
+            helper(n, st, new StringBuilder(output).append(')'), OB, CB + 1);
+        }
+    }
+}
