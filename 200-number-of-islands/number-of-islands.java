b76383da9fca03fa9f65d31ff92a39dabc4d8e2b
UditSinha04 <sinha.udit04@gmail.com>
2025-04-20 11:55:46 +0530

    Time: 2 ms (99.77%) | Memory: 49.6 MB (52.76%) - LeetSync

--- 200-number-of-islands/number-of-islands.java
+++ 200-number-of-islands/number-of-islands.java
@@ -1,4 +1,5 @@
 class Solution {
+    
     public int numIslands(char[][] grid) {
         if (grid == null || grid.length == 0) return 0;
         int count = 0;
@@ -8,21 +9,21 @@ class Solution {
             for (int j = 0; j < n; j++) {
                 if (grid[i][j] == '1') {
                     count++;
-                    dfs(grid, i, j);
+                    dfs(grid, i, j, m, n);
                 }
             }
         }
         return count;
     }
 
-    private void dfs(char[][] grid, int i, int j) {
-        if (i < 0 || j < 0 || i >= grid.length || j >= grid[0].length || grid[i][j] != '1') return;
+    private void dfs(char[][] grid, int i, int j, int m, int n) {
+        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] != '1') return;
         
-        grid[i][j] = '0'; // Mark as visited
+        grid[i][j] = '0';
         
-        dfs(grid, i + 1, j); // Down
-        dfs(grid, i - 1, j); // Up
-        dfs(grid, i, j + 1); // Right
-        dfs(grid, i, j - 1); // Left
+        dfs(grid, i + 1, j, m, n); 
+        dfs(grid, i - 1, j, m, n); 
+        dfs(grid, i, j + 1, m, n); 
+        dfs(grid, i, j - 1, m, n); 
     }
 }
