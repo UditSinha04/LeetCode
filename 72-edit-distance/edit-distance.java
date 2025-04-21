f1eb3d88102f1808449ea2d5c2a1bd9564a7cd0f
UditSinha04 <sinha.udit04@gmail.com>
2025-04-21 11:40:56 +0530

    Time: 2 ms (100.00%) | Memory: 45 MB (62.88%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -1,21 +1,28 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int m = word1.length(), n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
+        int n1 = word1.length();
+        int n2 = word2.length();
+        int[][] dp = new int[n1][n2];
+        for(int[] r : dp){
+            Arrays.fill(r , -1);
+        }
+        return f(n1-1 , n2-1, word1 , word2 , dp);
+    }
 
-        for (int i = 0; i <= m; i++) dp[i][0] = i;
-        for (int j = 0; j <= n; j++) dp[0][j] = j;
+    public int f(int ind1 , int ind2 , String s1 , String s2 , int[][] dp){
 
-        for (int i = 1; i <= m; i++) {
-            for (int j = 1; j <= n; j++) {
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)) {
-                    dp[i][j] = dp[i - 1][j - 1]; 
-                } else {
-                    dp[i][j] = Math.min(dp[i - 1][j - 1], Math.min(dp[i - 1][j], dp[i][j - 1])) + 1; 
-                }
-            }
+        if(ind1 < 0){
+            return ind2 + 1;
+        }
+        if(ind2 < 0 ){
+            return ind1 + 1;
+        }
+        if(dp[ind1][ind2] != -1) return dp[ind1][ind2];
+        if(s1.charAt(ind1) == s2.charAt(ind2)){
+            return dp[ind1][ind2] =  f(ind1 - 1 , ind2 - 1 , s1 , s2 , dp);
+        }else{
+            return dp[ind1][ind2] = 1 + Math.min(f(ind1 - 1, ind2 - 1, s1 , s2 , dp) , Math.min(f(ind1 , ind2 - 1 , s1 , s2 , dp) , f(ind1 - 1 , ind2 , s1 , s2 , dp)));
         }
 
-        return dp[m][n];
     }
 }
