1968a25f37d3adf5c38aebf9fe9a3007bf86e230
UditSinha04 <sinha.udit04@gmail.com>
2025-04-21 10:28:35 +0530

    Time: 0 ms (100.00%) | Memory: 41 MB (77.03%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,6 +1,6 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length == 0) return 0;
+        //if(nums.length == 0) return 0;
         if(nums.length == 1) return nums[0];
 
         int dp[] = new int[nums.length];
