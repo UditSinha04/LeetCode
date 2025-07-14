47069766a5275bfaf08f897dcfb058c74c4b0c7d
Udit Sinha <sinha.udit04@gmail.com>
2025-07-15 00:25:21 +0530

    Time: 13 ms (42.40%) | Memory: 44.7 MB (46.69%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        String cleaned = s.replaceAll("[^a-zA-Z0-9]", "");
+        String rev = new StringBuilder(cleaned).reverse().toString();
+
+        if(rev.equalsIgnoreCase(cleaned)) return true;
+        
+        return false;
+    }
+}
